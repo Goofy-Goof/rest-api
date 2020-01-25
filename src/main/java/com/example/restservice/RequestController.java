1c21ba183084f47c309913b2c2f44729b8c69811
artem sereda <artem.sereda.1503@campus.tu-berlin.de>
2020-01-26 00:17:31 +0100

    removed unused imports

--- src/main/java/com/example/restservice/RequestController.java
+++ src/main/java/com/example/restservice/RequestController.java
@@ -9,7 +9,6 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 import org.bson.conversions.Bson;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
--- src/test/java/com/example/restservice/RequestControllerTest.java
+++ src/test/java/com/example/restservice/RequestControllerTest.java
@@ -1,49 +1,21 @@
-/*
- * Copyright 2016 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *	  https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.restservice;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.jayway.jsonpath.JsonPath;
 import static org.junit.Assert.*;
-
-import org.bson.Document;
-import org.bson.conversions.Bson;
 import org.junit.Ignore;
 import org.junit.Test;
-
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.junit.runner.RunWith;
-
-import java.net.MalformedURLException;
 import java.util.LinkedList;
 import java.util.Random;
 import org.apache.commons.lang.*;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import javax.validation.constraints.AssertTrue;
+
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(RequestController.class)
